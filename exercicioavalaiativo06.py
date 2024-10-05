18e082563b974e433b237fe31d6425a4199c7b4f
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-04 21:09:31 -0300

    Add files via upload

---
+++ exercicioavalaiativo06.py
@@ -0,0 +1,7 @@
+# Potência de um número
+numero = int(input("Digite o número:"))
+expoente = int(input("Digite o expoente:"))
+
+potencia = (numero ** expoente)
+
+print(" A potência é:\n", potencia)
